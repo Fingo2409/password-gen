bec6602c984b7ebd6f8312f1a2a36db6fb5ad4f7
Joshua Schrader <schrader.joshua@icloud.com>
2021-03-21 12:13:04 +0100

    Initial Commit

---
+++ password-gen/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  password-gen
+//
+//  Created by Joshua Schrader on 21.03.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
